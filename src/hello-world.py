9f9b836104fa57493bbc4d5b2c753591872b3d3f
Rodrigo Carcuz <Rodrigo Carcuz@DESKTOP-9IEPFJS>
2019-07-30 21:50:52 -0600

    commitfin

--- src/hello-world.py
+++ src/hello-world.py
@@ -1,5 +1,6 @@
 import os
-print("RODRIGO EDUARDO CARCUZ ORTEGA")
+print("RODRIGO EDUARDO CARCUZ ORTEGA 201700633")
+
 print("lista enlazada simple ")
  #lista enlazada en python#
 class Nodo:
@@ -98,6 +99,6 @@ listas = Lista()
 		else: 
 			print("")
 			input("pulsa una opcion valida")
-			
+
 
 				
